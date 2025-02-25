9cdc5eefb2485535e1df9805ca090cb228df56be
Ali Moghimi <mghmiali@gmail.com>
2025-02-25 17:45:07 +0000

    fix: add logging messages

--- db_backends/azure_postgres/base.py
+++ db_backends/azure_postgres/base.py
@@ -1,3 +1,5 @@
+import logging
+
 import psycopg2
 from azure.identity import DefaultAzureCredential
 from django.conf import settings
@@ -5,15 +7,20 @@ from django.db.backends.postgresql.base import \
     DatabaseWrapper as BaseDatabaseWrapper
 
 
+logger = logging.getLogger(__name__)
+
+
 class DatabaseWrapper(BaseDatabaseWrapper):
     def get_new_connection(self, conn_params):
         """Override connection method to support both authentication types."""
 
         if settings.SQL_AD_LOGIN:
+            logger.info("POSTGRES AD LOGIN AUTH")
             credential = DefaultAzureCredential()
             token = credential.get_token(
                 "https://ossrdbms-aad.database.windows.net/.default"
             ).token
+            logger.info(f"FETCHED TOKEN FROM AZURE: {token}")
             conn_params["password"] = token
             conn_params["user"] = settings.DATABASES["default"]["USER"]
             conn_params["sslmode"] = "require"
