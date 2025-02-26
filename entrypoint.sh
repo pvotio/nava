227cf2e3178319a579772cc8761f5194ecc844e6
Ali Moghimi <mghmiali@gmail.com>
2025-02-26 10:43:33 +0000

    fix: add env logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,10 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 if [ "$DJANGO_ENV" = "production" ]; then
+    echo "PRODUCTION ENV"
     gunicorn config.wsgi:application --bind 0.0.0.0:8000
 else
+    echo "DEBUG ENV"
     python manage.py runserver 0.0.0.0:8000
 fi
 
