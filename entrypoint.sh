4ed5cd684a71f3b7f68bf7dcdc466347c4214c41
clemstruck <69113110+clemstruck@users.noreply.github.com>
2025-04-16 14:26:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,10 @@ fi
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# patch by Stefan: Mount PVC to /tmp/static and use emptyDir for /app/static. Quirk to handle missing chmod permissions on PVC. Copy all files after collection.
+rm -rf /tmp/static/*
+cp -R /app/static/* /tmp/static
+
 if [ "$DJANGO_ENV" = "production" ]; then
     echo "PRODUCTION ENV"
     gunicorn config.wsgi:application --bind 0.0.0.0:8000
